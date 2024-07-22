50cfcf88dbb6fbae58d4d93d9089cb60f6bcd099
bjaouiala <bjaouiala3@gmail.com>
2024-07-23 00:27:29 +0100

    config consumer to recive message from the topic

--- kafka-demo/src/main/java/com/ala/kafka/consumer/KafkaConsumer.java
+++ kafka-demo/src/main/java/com/ala/kafka/consumer/KafkaConsumer.java
@@ -1,2 +1,17 @@
-package com.ala.kafka.consumer;public class KafkaConsumer {
+package com.ala.kafka.consumer;
+
+import lombok.extern.java.Log;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import static java.lang.String.*;
+
+@Service
+@Slf4j
+public class KafkaConsumer {
+    @KafkaListener(topics = "first",groupId = "myGroup")
+    public void consumeMessage(String message){
+        log.info(format("consuming message from first topic :: %s",message));
+    }
 }
