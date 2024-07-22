5bbc31f3bf4914b9f94c568c9fd0a4d5e2754b2a
bjaouiala <bjaouiala3@gmail.com>
2024-07-23 00:02:44 +0100

    producer controller

--- kafka-demo/src/main/java/com/ala/kafka/controller/MessageController.java
+++ kafka-demo/src/main/java/com/ala/kafka/controller/MessageController.java
@@ -1,2 +1,21 @@
-package com.ala.kafka.controller;public class MessageController {
+package com.ala.kafka.controller;
+
+import com.ala.kafka.producer.KafkaProducer;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/message")
+public class MessageController {
+    private final KafkaProducer kafkaProducer;
+    @PostMapping
+    public ResponseEntity<String> sendMessage(@RequestBody String message){
+        kafkaProducer.sendMessage(message);
+        return ResponseEntity.ok("message sent succefully");
+    }
 }
